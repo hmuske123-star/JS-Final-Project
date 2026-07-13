d743c1f90f4582b4bc8dfb80fa303d65ec160bc2
hmuske123-star <hmuske123@gmail.com>
2026-07-13 08:26:33 -0700

    Navigation

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 /* Harvard Art Museums API Key:
    0705d3ec-7bc9-4cc0-bd62-2302fbd3f04
+*/
+
+function openMenu() {
+   document.body.classList += ("menu--open");
+}
+
+function closeMenu() {
+   document.body.classList.remove("menu--open");
+}
