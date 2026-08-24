6dd947b8e5d737be74c881a11a6e83b4e47e6df8
hmuske123-star <hmuske123@gmail.com>
2026-08-24 16:11:49 -0700

    API Integration

--- app.js
+++ app.js
@@ -12,10 +12,27 @@ function closeMenu() {
    document.querySelector(".close-btn").classList.remove("show");
 }
 
-async function fetchData() {
-   const images = await fetch("https://api.harvardartmuseums.org/image?apikey=0705d3ec-7bc9-4cc0-bd62-23302fbd3f04");
-   const imageData = await images.json();
-   console.log(imageData);
+const artworksWrapper = document.querySelector(".artworks");
+
+async function fetchArtworks(searchTerm) {
+   const artworks = await fetch(`https://api.artic.edu/api/v1/artworks?keyword=${searchTerm}`);
+   const data = await artworks.json();
+   console.log(data);
+   artworksWrapper.innerHTML = data.data.map((artworks) => {
+      return `<div class='images'>
+      <img src="https://www.artic.edu/iiif/2/${artworks.image_id}/full/843,/0/default.jpg">
+      <h2>${artworks.title}</h2>
+      <h4>${artworks.artist_title}</h4>
+      <h4>${artworks.artwork_type_title}</h4>
+      <h4>${artworks.department_title}</h4>
+      <h4>${artworks.date_display}</h4>
+      <button>Learn More</button>
+      </div>`;
+   });
 }
 
-fetchData();
+
+function onSearchChange(event) {
+   console.log(event.target.value);
+   fetchArtworks(event.target.value);
+}
